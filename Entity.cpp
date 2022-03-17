9c862b59e113e87377662c18b1308af3d23ec110
Héctor Báscones <34044086+Hekbas@users.noreply.github.com>
2022-03-17 19:12:45 +0100

    Add files via upload

--- Entity.cpp
+++ Entity.cpp
@@ -1,5 +1,4 @@
-#include "Entity.h"
-
+#include "Game.h"
 
 Entity::Entity()
 {
@@ -8,7 +7,7 @@ Entity::Entity()
 Entity::~Entity()
 {
 }
-void Entity::Init(int posx, int posy, int w, int h, int s) 
+void Entity::Init(int posx, int posy, int w, int h, int s)
 {
 	x = posx;
 	y = posy;
@@ -17,6 +16,32 @@ void Entity::Init(int posx, int posy, int w, int h, int s)
 	speed = s;
 	is_alive = true;
 }
+void Entity::InitPacman(int x, int y, int vx, int vy, int vxTurn, int vyTurn, bool is_alive)
+{
+	this->x = x;
+	this->y = y;
+	this->vx = vx;
+	this->vy = vy;
+	this->vxTurn = vxTurn;
+	this->vyTurn = vyTurn;
+	this->is_alive = is_alive;
+
+	width = 32;
+	height = 32;
+}
+void Entity::InitGhost(int x, int y, int vx, int vy, bool is_alive, bool chase, char posOld)
+{
+	this->x = x;
+	this->y = y;
+	this->vx = vx;
+	this->vy = vy;
+	this->is_alive = is_alive;
+	this->chase = chase;
+	this->posOld = posOld;
+
+	width = 32;
+	height = 32;
+}
 void Entity::GetRect(int *posx, int *posy, int *w, int *h)
 {
 	*posx = x;
@@ -24,27 +49,41 @@ void Entity::GetRect(int *posx, int *posy, int *w, int *h)
 	*w = width;
 	*h = height;
 }
-int Entity::GetX()
+void Entity::GetRectPacman(int* posx, int* posy, int* w, int* h)
 {
-	return x;
-}
-int Entity::GetWidth()
-{
-	return width;
-}
-void Entity::SetX(int posx)
-{
-	x = posx;
-}
-void Entity::ShutDown()
-{
-	is_alive = false;
-}
-bool Entity::IsAlive()
-{
-	return is_alive;
+	*posx = x*32;
+	*posy = y*32;
+	*w = width;
+	*h = height;
 }
 
+int Entity::GetX() { return x; }
+void Entity::SetX(int x) { this->x = x; }
+
+int Entity::GetY() { return y; }
+void Entity::SetY(int y) { this->y = y; }
+
+int Entity::GetVx() { return vx; }
+void Entity::SetVx(int vx) { this->vx = vx; }
+
+int Entity::GetVy() { return vy; }
+void Entity::SetVy(int vy) { this->vy = vy; }
+
+int Entity::GetVxTurn() { return vxTurn; }
+void Entity::SetVxTurn(int vxTurn) { this->vxTurn = vxTurn; }
+
+int Entity::GetVyTurn() { return vyTurn; }
+void Entity::SetVyTurn(int vyTurn) { this->vyTurn = vyTurn; }
+
+bool Entity::GetChase() { return chase; }
+
+char Entity::GetPosOld() { return posOld; }
+void Entity::SetPosOld(char posOld) { this->posOld = posOld; }
+
+int Entity::GetWidth() { return width; }
+
+void Entity::ShutDown() { is_alive = false; }
+bool Entity::IsAlive() { return is_alive; }
 void Entity::Move(int dx, int dy)
 {
 	x += dx * speed;
